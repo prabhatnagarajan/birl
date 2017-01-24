c953a6d06be7d82b48d74a74269af36b20bf9f37
Prabhat <prabhat.nagarajan@gmail.com>
2017-01-24 14:50:15 -0600

    writes plan in gridworld file

---
+++ gridworld.py
@@ -0,0 +1,12 @@
+'''
+- make a gridworld an environment
+- make it have states
+- make it have transition function
+- make it have a reward function
+- make grid states have features
+- make a demonstration class that stores a trajectory
+- keep a list of demonstrations
+- have gridworld have a record function that takes in number of demos to record
+- MDP have states, transitions, rewards, record function
+- Make instances of MDP like gridworld have record function
+''' 
