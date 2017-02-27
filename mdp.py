b2c6eb8c4fee427b74af9ea1067d7b94936db751
Prabhat <prabhat.nagarajan@gmail.com>
2017-02-26 22:55:38 -0600

    completing task

--- mdp.py
+++ mdp.py
@@ -1,141 +1,69 @@
-"""
-Copyright (c) 2016 aima-python contributors
-
-This file is an adaptation of the aima-python mdp file.
-
-Markov Decision Processes (Chapter 17)
-
-First we define an MDP, and the special case of a GridMDP, in which
-states are laid out in a 2-dimensional grid.  We also represent a policy
-as a dictionary of {state:action} pairs, and a Utility function as a
-dictionary of {state:number} pairs.  We then define the value_iteration
-and policy_iteration algorithms."""
-
-from utils import argmax, vector_add, print_table
-from grid import orientations, turn_right, turn_left
+#!/usr/bin/env python
 import numpy as np
-import random
-
-
+from pdb import set_trace
 class MDP:
+	def __init__(self, transitions, rewards):
+		self.transitions = transitions
+		num_actions = np.shape(transitions)[1]
+		self.rewards = rewards
+         
+	'''Policy Iteration from Sutton and Barto
+	   assumes discount factor of 0.99
+	   Deterministic policy iteration
+	'''
+	def policy_iteration(self, policy=None):
+		print "Enter Policy Iteration"
+		#initialization
+		if policy is None:
+			policy = np.zeros(np.shape(self.transitions)[0],dtype=np.int8)
 
-    """A Markov Decision Process, defined by an initial state, transition model,
-    and reward function. We also keep track of a gamma value, for use by
-    algorithms. The transition model is represented somewhat differently from
-    the text.  Instead of P(s' | s, a) being a probability number for each
-    state/state/action triplet, we instead have T(s, a) return a
-    list of (p, s') pairs.  We also keep track of the possible states,
-    terminal states, and actions for each state. [page 646]"""
+		policy_stable = False
+		count = 0
+		while not policy_stable:
+			#policy evaluation
+			V = self.policy_evaluation(policy)
+			print count
+			count += 1
+			diff_count = 0
+			#policy improvement
+			policy_stable = True
+			for state in range(np.shape(self.transitions)[0]):
+				old_action = policy[state]
+				action_vals = np.dot(self.transitions[state,:,:], self.rewards + 0.99 * V).tolist()
+				policy[state] = action_vals.index(max(action_vals))
+				if not old_action == policy[state]:
+					diff_count += 1
+					policy_stable = False
+			print "Diff count is"
+			print diff_count
+		print "policy is"
+		print policy
+		print "returning"
+		return (policy, V)
 
-    def __init__(self, init, actlist, terminals, gamma, states, reward, transitions):
-        self.init = init
-        self.actlist = actlist
-        self.terminals = terminals
-        if not (0 <= gamma < 1):
-            raise ValueError("An MDP must have 0 <= gamma < 1")
-        self.gamma = gamma
-        self.states = states
-        self.reward = reward
-        self.transitions = transitions
-        self.Transition = [[[(self.transitions[state, action, next_s], next_s) for next_s in states] for action in actlist] for state in states]
-        for state in states:
-            for action in actlist:
-                sum_val = 0
-                for next_s in states:
-                    sum_val += self.Transition[state][action][next_s][0]
-                print sum_val
-                # print self.Transition[state][action]
-                # print ""
+	'''
+	policy - deterministic policy, maps state to action
+	-Deterministic policy evaluation
+	'''
+	def policy_evaluation(self, policy):
+		print "Policy Evaluation"
+		V = np.zeros(np.shape(self.transitions)[0])
+		delta = 1
+		while delta > 0.01:
+			delta = 0
+			for state in range(len(V)):
+				value = V[state]
+				V[state] = np.dot(self.transitions[state,:,:], self.rewards + 0.99 * V)[policy[state]]
+				delta = max(delta, abs(value - V[state]))
+		return V
 
-    def R(self, state):
-        "Return a numeric reward for this state."
-        return self.reward[state]
+	def q_evaluation(self, policy):
+		print "Evaluating Q"
+		V = self.policy_evaluation(policy)
+		Q = np.zeros(np.shape(self.transitions)[0:2])
+		print np.shape(Q)
+		return Q
 
-    def T(self, state, action):
-        """Transition model.  From a state and an action, return a list
-        of (probability, result-state) pairs."""
-        return self.Transition[state, action]
-
-    def actions(self, state):
-        """Set of actions that can be performed in this state.  By default, a
-        fixed list of actions, except for terminal states. Override this
-        method if you need to specialize by state."""
-        if state in self.terminals:
-            return [None]
-        else:
-            return self.actlist
-# ______________________________________________________________________________
-
-
-def value_iteration(mdp, epsilon=0.001):
-    "Solving an MDP by value iteration. [Figure 17.4]"
-    U1 = {s: 0 for s in mdp.states}
-    R, T, gamma = mdp.R, mdp.T, mdp.gamma
-    while True:
-        U = U1.copy()
-        delta = 0
-        for s in mdp.states:
-            U1[s] = R(s) + gamma * max([sum([p * U[s1] for (p, s1) in T(s, a)])
-                                        for a in mdp.actions(s)])
-            delta = max(delta, abs(U1[s] - U[s]))
-        if delta < epsilon * (1 - gamma) / gamma:
-            return U
-
-
-def best_policy(mdp, U):
-    """Given an MDP and a utility function U, determine the best policy,
-    as a mapping from state to action. (Equation 17.4)"""
-    pi = {}
-    for s in mdp.states:
-        pi[s] = argmax(mdp.actions(s), key=lambda a: expected_utility(a, s, U, mdp))
-    return pi
-
-
-def expected_utility(a, s, U, mdp):
-    "The expected utility of doing a in state s, according to the MDP and U."
-    return sum([p * U[s1] for (p, s1) in mdp.T(s, a)])
-
-# ______________________________________________________________________________
-
-
-def policy_iteration(mdp):
-    "Solve an MDP by policy iteration [Figure 17.7]"
-    U = {s: 0 for s in mdp.states}
-    pi = {s: random.choice(mdp.actions(s)) for s in mdp.states}
-    while True:
-        U = policy_evaluation(pi, U, mdp)
-        unchanged = True
-        for s in mdp.states:
-            a = argmax(mdp.actions(s), key=lambda a: expected_utility(a, s, U, mdp))
-            if a != pi[s]:
-                pi[s] = a
-                unchanged = False
-        if unchanged:
-            return pi
-
-
-def policy_evaluation(pi, U, mdp, k=20):
-    """Return an updated utility mapping U from each state in the MDP to its
-    utility, using an approximation (modified policy iteration)."""
-    R, T, gamma = mdp.R, mdp.T, mdp.gamma
-    for i in range(k):
-        for s in mdp.states:
-            U[s] = R(s) + gamma * sum([p * U[s1] for (p, s1) in T(s, pi[s])])
-    return U
-
-__doc__ += """
->>> pi = best_policy(sequential_decision_environment, value_iteration(sequential_decision_environment, .01))
-
->>> sequential_decision_environment.to_arrows(pi)
-[['>', '>', '>', '.'], ['^', None, '^', '.'], ['^', '>', '^', '<']]
-
->>> print_table(sequential_decision_environment.to_arrows(pi))
->   >      >   .
-^   None   ^   .
-^   >      ^   <
-
->>> print_table(sequential_decision_environment.to_arrows(policy_iteration(sequential_decision_environment)))
->   >      >   .
-^   None   ^   .
-^   >      ^   <
-"""
+class MDPR:
+	def __init__(self, transitions):
+		self.transitions = transitions
